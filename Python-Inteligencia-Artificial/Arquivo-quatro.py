b09fc7342b0f8fc058b26d4af4c850e56ea2aec8
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-10 23:41:34 -0300

    teste funcao

---
+++ Python-Inteligencia-Artificial/Arquivo-quatro.py
@@ -0,0 +1,7 @@
+def soma(numero1, numero2):
+    return numero1 + numero2
+
+numberOne = int(input("Enter Number One:  "))
+numberTwo = int(input("Enter Number Two:  "))
+
+print(soma(numberOne, numberTwo))
