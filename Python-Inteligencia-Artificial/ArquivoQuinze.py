f92f86fb2ebb23cdabc8d2aeb51984a8ab58fded
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-11 17:46:24 -0300

    Heranca multipla

---
+++ Python-Inteligencia-Artificial/ArquivoQuinze.py
@@ -0,0 +1,18 @@
+
+#Multiple Inheritance
+class Swimmer:
+    def swim(self):
+        return "Swim in the pool"
+
+class Cyclist:
+    def cycling(self):
+        return "Cycling on the road"
+
+
+class Triathlete(Swimmer, Cyclist):
+    def running_marathon(self):
+        return "Running in the marathon"
+
+
+athlete = Triathlete()
+print(f" Triathlete: {athlete.running_marathon()}, Swimmer: {athlete.swim()}, Cyclist: {athlete.cycling()}")
