767cc42f772b7f1e3b22380729da57a92c3f892a
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-10 23:53:30 -0300

    teste dicionarios

---
+++ Python-Inteligencia-Artificial/Arquivo-cinco.py
@@ -0,0 +1,15 @@
+pessoa =  {
+    "nome":"Lucas",
+    "Departamento":"TI",
+    "Salario" : 50000,
+}
+pessoa['cidade'] = "São Paulo"
+print(pessoa)
+
+aluno = {
+    'nome': input("nome"),
+    'idade': int(input("Idade")),
+    'nota': float(input("nota")),
+}
+
+print(aluno)
