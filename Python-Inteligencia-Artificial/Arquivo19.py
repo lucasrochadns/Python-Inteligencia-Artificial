6c320210c603952a45cb17a6d637238870928f3d
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-13 02:56:05 -0300

    Lambda

---
+++ Python-Inteligencia-Artificial/Arquivo19.py
@@ -0,0 +1,9 @@
+
+
+listaNumberOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+listaNumberTwo = [x for x in listaNumberOne if x % 2 == 0]
+print(listaNumberTwo)
+
+listThree = ['Banana', 'Abacate', 'Pera', 'Uva', 'Abacaxi']
+listFour = [x for x in listThree if 'A' in x]
+print(listFour)
